21b5938a12405dc0334c49338cc2d7e19e14e8a2
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-01 13:14:52 +0530

    Update and rename lineage_blossom.mk to orion_blossom.mk

--- orion_blossom.mk
+++ orion_blossom.mk
@@ -12,22 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common OrionOS stuff.
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 scr_resolution := 720
 
-# Matrixx stuff.
-MATRIXX_MAINTAINER := AsTechpro20
-MATRIXX_CHIPSET := MT6765
-MATRIXX_BATTERY := 5000mAh
-MATRIXX_DISPLAY := 720x1600
-USE_PIXEL_CHARGING := true
+#OrionOS stuffs.
+ORION_MAINTAINER := AsTechpro20
+ORION_MAINTAINER_LINK := https://github.com/AsTechpro20
+ORION_GAPPS := false
+TARGET_ENABLE_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_BLUR := true
 
-PRODUCT_NAME := lineage_blossom
+PRODUCT_NAME := orion_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
